fc4fe35f079291362a6961539127fa8b7f10af80
Fantazindy-KR <leehyen4965@gmail.com>
2022-02-23 10:37:05 +0900

    Add detail page

---
+++ src/components/Detail.js
@@ -0,0 +1,145 @@
+import React from "react";
+import styled from "styled-components";
+
+function Detail() {
+  return (
+    <Container>
+      <Background>
+        <img src="https://cdn.wallpapersafari.com/79/81/sCDgJ1.jpg" />
+      </Background>
+      <ImageTitle>
+        <img src="https://www.seekpng.com/png/full/20-200510_frozen-musical-logo-frozen-logo-png.png" />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" />
+        </GroupWatchButton>
+      </Controls>
+      <SubTitle>2013 American computer-animated musical fantasy film</SubTitle>
+      <Description>
+        Frozen is a 2013 American computer-animated musical fantasy film
+        produced by Walt Disney Animation Studios and released by Walt Disney
+        Pictures.[5] The 53rd Disney animated feature film, it is inspired by
+        Hans Christian Andersen's fairy tale "The Snow Queen". The film depicts
+        a princess who sets off on a journey alongside an iceman, his reindeer,
+        and a snowman to find her estranged sister, whose icy powers have
+        inadvertently trapped their kingdom in eternal winter.
+      </Description>
+    </Container>
+  );
+}
+
+export default Detail;
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  width: 35vw;
+  min-height: 170px;
+  min-width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 0px 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background: rgb(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const GroupWatchButton = styled(AddButton)`
+  background: rgba(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 18px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
+
+const Description = styled.div`
+  width: 50vw;
+  line-height: 1.4;
+  font-size: 14px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
+`;
